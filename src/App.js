0cd861f7429578c43b1f5363da11a5145baf4435
Amardeep Mann <amar2slick@gmail.com>
2023-04-25 09:07:16 -0700

    Templated routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/SignUp' element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
