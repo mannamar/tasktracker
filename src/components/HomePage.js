6fdd5d285d21280dc2f81c0f93a46f00d3c13e3c
carolinehanaa <carolinehana28@gmail.com>
2023-04-25 10:00:51 -0700

    body background

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./HomePage.css"
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <div>
+            <nav class="navbar nav-bg ">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">Navbar</span>
+                    <button type="button" class="btn btn-dark">Dark</button>
+                </div>
+            </nav>
+        </div>
     )
 }
