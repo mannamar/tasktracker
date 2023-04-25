a1a174e76d59878e98ebce44d81f3cab60a81592
carolinehanaa <carolinehana28@gmail.com>
2023-04-25 14:38:41 -0700

    cards

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,29 +2,37 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./HomePage.css"
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import plus from "../Assets/Plus.png";
 
 export default function HomePage() {
     return (
         <div>
             <nav className="navbar nav-bg ">
                 <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Task Tracker</span>
+                    <span className="nav-title mb-0 h1">Task Tracker</span>
                     <button type="button" className="btn-bg ">Log Out</button>
                 </div>
             </nav>
 
             <Container>
                 <Row className="field-spacing">
-                    <Card className="md-col-3"> 
-                        <Card.Header className="to-do">To Do . . .</Card.Header>
+                    <Card className="md-col-4"> 
+                        <Card.Header className="to-do">To Do . . .
+                        <img className="plus" src={plus} />
+                        </Card.Header>
+                        <Card.Body>This is some text within a card body.</Card.Body>
+                        
+                    </Card>
+                    <Card className="md-col-4">
+                        <Card.Header className="to-do">In Progress . . .
+                        <img className="plus" src={plus} />
+                        </Card.Header>
                         <Card.Body>This is some text within a card body.</Card.Body>
                     </Card>
-                    <Card className="md-col-3">
-                        <Card.Header className="to-do">To Do . . .</Card.Header>
-                        <Card.Body>This is some text within a card body.</Card.Body>
-                    </Card>
-                    <Card className="md-col-3">
-                        <Card.Header className="to-do">To Do . . .</Card.Header>
+                    <Card className="md-col-4">
+                        <Card.Header className="to-do">Done . . .
+                        <img className="plus" src={plus} />
+                        </Card.Header>
                         <Card.Body>This is some text within a card body.</Card.Body>
                     </Card>
                     {/* <Col className="to-do">To Do . . . 
