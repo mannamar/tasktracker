a2ee6b285f19589dafffca94277dbfec5f263e34
carolinehanaa <carolinehana28@gmail.com>
2023-04-25 12:01:01 -0700

    fields

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,47 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./HomePage.css"
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 export default function HomePage() {
     return (
         <div>
-            <nav class="navbar nav-bg ">
-                <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1">Navbar</span>
-                    <button type="button" class="btn btn-dark">Dark</button>
+            <nav className="navbar nav-bg ">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Task Tracker</span>
+                    <button type="button" className="btn-bg ">Log Out</button>
                 </div>
             </nav>
+
+            <Container>
+                <Row className="field-spacing">
+                    <Card className="md-col-3"> 
+                        <Card.Header className="to-do">To Do . . .</Card.Header>
+                        <Card.Body>This is some text within a card body.</Card.Body>
+                    </Card>
+                    <Card className="md-col-3">
+                        <Card.Header className="to-do">To Do . . .</Card.Header>
+                        <Card.Body>This is some text within a card body.</Card.Body>
+                    </Card>
+                    <Card className="md-col-3">
+                        <Card.Header className="to-do">To Do . . .</Card.Header>
+                        <Card.Body>This is some text within a card body.</Card.Body>
+                    </Card>
+                    {/* <Col className="to-do">To Do . . . 
+                    <Col className="to-do-body">body
+                    </Col>
+                    </Col>
+
+                    <Col>In Progress . . . 
+                    <Col> body
+                    </Col>
+                    </Col>
+                    <Col>Done . . .
+                    <Col>Body
+                    </Col>
+                     </Col> */}
+                </Row>
+            </Container>
         </div>
     )
 }
